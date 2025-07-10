49520738ae017e13feef0a7e46854eb7be5e427a
dimas07076 <harryderecife@gmail.com>
2025-07-10 19:54:58 +0000

    Mensagem

--- shared/types/express/index.d.ts
+++ shared/types/express/index.d.ts
@@ -1,31 +1,11 @@
 // shared/types/express/index.d.ts
-
-// Interfaces para os payloads dos tokens JWT
-// Definimos aqui como a fonte da verdade para todo o projeto.
-interface PersonalTrainerPayload {
-    id: string;
-    role: 'personal' | 'admin';
-    firstName: string;
-    lastName: string;
-    email: string; // ---> CORREÇÃO CRÍTICA: Campo adicionado para sincronizar com o middleware
+interface UserPayload {
+  id: string;
+  role: string;
 }
 
-interface AlunoPayload {
-    id:string;
-    role: 'aluno';
-    nome: string;
-}
-
-// Declaração global para estender a interface Request do Express
-// Agora o TypeScript saberá que 'user' E 'aluno' podem existir no objeto 'req'
-declare global {
-  namespace Express {
-    interface Request {
-      user?: PersonalTrainerPayload;
-      aluno?: AlunoPayload;
-    }
+declare namespace Express {
+  export interface Request {
+    user?: UserPayload;
   }
 }
-
-// Linha necessária para garantir que este arquivo seja tratado como um módulo.
-export {};
